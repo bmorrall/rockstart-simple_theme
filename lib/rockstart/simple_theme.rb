c7ec6d9cbcb06d121ef1786ea775561ba5210c43
Ben Morrall <bemo56@hotmail.com>
2020-04-03 14:56:31 -0700

    Code quality cleanup

--- lib/rockstart/simple_theme.rb
+++ lib/rockstart/simple_theme.rb
@@ -1,6 +1,9 @@
+# frozen_string_literal: true
+
 require "rockstart/simple_theme/railtie"
 
 module Rockstart
+  # Generators for a Simple Theme built with Tailwind CSS
   module SimpleTheme
     # Your code goes here...
   end
--- lib/rockstart/simple_theme/railtie.rb
+++ lib/rockstart/simple_theme/railtie.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Rockstart
   module SimpleTheme
     class Railtie < ::Rails::Railtie
