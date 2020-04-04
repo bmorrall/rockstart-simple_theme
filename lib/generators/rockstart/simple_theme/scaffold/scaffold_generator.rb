e711e4dddab413df25821d8a99a7032c13fcbb0d
Ben Morrall <bemo56@hotmail.com>
2020-04-03 18:10:14 -0700

    Add scaffold for index action

--- lib/generators/rockstart/simple_theme/scaffold/scaffold_generator.rb
+++ lib/generators/rockstart/simple_theme/scaffold/scaffold_generator.rb
@@ -4,6 +4,7 @@ class Rockstart::SimpleTheme::ScaffoldGenerator < Rails::Generators::Base
   source_root File.expand_path("templates", __dir__)
 
   def install_scaffold_templates
+    copy_file "index.html.erb", "lib/templates/erb/scaffold/index.html.erb"
     copy_file "show.html.erb", "lib/templates/erb/scaffold/show.html.erb"
   end
 end
